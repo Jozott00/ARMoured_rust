2e207267da640e870fdbdf01e4efb3d11132c0fa
Johannes Zottele <official@johannes-zottele.at>
2023-06-05 20:27:52 +0200

    add bad64 disassembly

--- src/instruction_stream.rs
+++ src/instruction_stream.rs
@@ -1,7 +1,8 @@
 pub mod data_proc_imm;
 pub mod ret;
 
-use std::mem;
+use std::{mem, slice};
+use bad64::disasm;
 use bit_seq::{bseq, bseq_32};
 use crate::instruction_emitter::InstrEmitter;
 use crate::mc_memory::McMemory;
@@ -47,5 +48,29 @@ impl<'mem> InstrStream<'mem> {
     fn emit(&mut self, instr: Instruction) {
         debug_assert!(!self.mem.is_executable(), "Cannot emit instruction while memory is in execution mode");
         self.emitter.emit(instr);
+    }
+
+    pub fn print_disasm(&self) {
+        let decoded_iter = disasm(self.written_memory(), self.base_ptr() as u64);
+        for instr in decoded_iter {
+            if let Ok(instr) = instr {
+                let encoding = instr.opcode().to_le_bytes();
+                let enc_str = encoding.map(|e| format!("{e:02x}")).join(" ");
+                println!("{:#x}: {enc_str}      {instr}", instr.address());
+            } else {
+                println!("! Incorrect instruction");
+            };
+        }
+    }
+
+    fn written_memory(&self) -> &[u8] {
+        let len = (self.emitter.instr_ptr() as usize) - (self.mem.addr() as usize);
+        let ptr = self.mem.addr() as *const u8;
+
+        assert_eq!(len % 4, 0, "Len is not a multiple of 4");
+        assert_eq!(ptr as usize % mem::align_of::<u32>(), 0, "Memory not u32 aligned");
+        assert!(self.mem.len() >= len, "Requested length exceeds memory map!");
+
+        unsafe { slice::from_raw_parts(ptr, len) }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod instruction_emitter;
 mod types;
 mod instruction_stream;
 
+use bad64::{decode, disasm};
 use crate::instruction_stream::InstrStream;
 use crate::mc_memory::McMemory;
 
@@ -10,12 +11,13 @@ use crate::mc_memory::McMemory;
 fn main() {
     let mut mem = McMemory::new_pagesize();
     let mut stream = InstrStream::new(&mut mem);
-    stream.mov_imm(0, 0x23);
+    stream.mov_64_imm(0, 0x23, 0.into());
     stream.ret();
 
     stream.patch_at(stream.base_ptr(), |s| {
-        s.mov_imm(0, 0x78);
+        s.mov_64_imm(0, 0x78, 0.into());
     });
+    stream.print_disasm();
 
     let func = stream.nullary_fn_ptr();
     mem.make_executable();
